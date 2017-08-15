2b618ef45c83b3316ad3a9085d52d0f282ccfc83
Esther Olatunde <olatundeesther@gmail.com>
2017-08-15 21:14:12 +0100

    - Get hbs_content advertisement and random tweets - Updates buffer schedule if empty and shuffle the load (quite incomplete - need to add test specs and handle errors)

--- lib/buffer_schedule.rb
+++ lib/buffer_schedule.rb
@@ -4,16 +4,31 @@ class BufferSchedule
 
   def initialize(client)
     @client = client
-    @profile_id = client.profiles[0].id
+
+    # Because I already know that @happybearsoft is the first profile - can be refactored to find the profile_id by @username
+    @profile_id = client.profiles.map(&:id).first
   end
 
-  def update(advertisement, random_content)
-    puts "is_empty" * 5
-    puts is_empty?
+  def is_empty?
+    scheduled_tweets.total == 0
+  end  
+
+  def update(tweets)
+    tweets.each do |tweet_text|
+      client.create_update(
+        body: {
+          text:
+            "#{tweet_text}",
+          profile_ids: [
+            "#{@profile_id}"
+          ]
+        },
+      )
+    end
   end
 
   def shuffle
-    
+    @client.shuffle_updates(@profile_id, {})   
   end
 
 
@@ -22,10 +37,4 @@ private
   def scheduled_tweets
     @client.updates_by_profile_id(@profile_id, { status: :pending})
   end
-
-  def is_empty?
-    scheduled_tweets.total == 0
-  end
-  
-
 end
