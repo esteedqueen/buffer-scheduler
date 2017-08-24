b78fceba25dd89eeb910ecb8004e809ed567cad4
Esther Olatunde <olatundeesther@gmail.com>
2017-08-24 20:12:10 +0100

    Replaces httparty with the ruby standard net/http for posting to zapier webhook and modifies logger format

--- Gemfile
+++ Gemfile
@@ -5,6 +5,5 @@ gem 'dotenv'
 gem 'buffer', github: 'bufferapp/buffer-ruby'
 gem 'contentful'
 gem 'hbs-content', '0.9.0', git: 'https://github.com/happybearsoftware/hbs-content.git'
-gem 'httparty'
 gem 'rspec'
 gem 'rubocop', '~> 0.49.1', require: false
--- lib/log_entry.rb
+++ lib/log_entry.rb
@@ -1,4 +1,8 @@
 require 'logger'
+require 'net/http'
+require 'net/https'
+require 'uri'
+require 'json'
 require 'dotenv'
 Dotenv.load
 
@@ -7,14 +11,25 @@ class LogEntry
     if @logger.nil?
       @logger = Logger.new 'debug.log'
       @logger.level = Logger::DEBUG
-      @logger.datetime_format = '%Y-%m-%d %H:%M:%S %Z '
+      @logger.formatter = proc do |severity, datetime, progname, msg|
+          date_format = datetime.strftime("%Y-%m-%d %H:%M:%S")       
+          "[ #{date_format} ] #{msg}\n"
+      end      
     end
     @logger
   end
 
   def self.send_to_zapier(log_output)
-    response = HTTParty.post(ENV['ZAPIER_LOG_OUTPUT_WEBHOOK'],
-                             body: { buffer_scheduler_log_output: log_output.to_s }).parsed_response
+    url = URI(ENV['ZAPIER_LOG_OUTPUT_WEBHOOK'])
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Post.new(url)
+    request['content-type'] = 'application/json'
+    request.body = { 'buffer_scheduler_log_output': log_output.to_s }.to_json
+    response = JSON.parse(http.request(request).body)
     File.delete('debug.log') if response['status'] == 'success'
   end
 end
