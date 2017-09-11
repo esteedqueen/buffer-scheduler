012b94fbdc388004b4b0ec66b3c54e3c622a1eb1
Esther Olatunde <olatundeesther@gmail.com>
2017-09-11 13:54:13 +0100

    Refactors code to pass rubocop's ABC size cop

--- lib/schedule_tweets_runner.rb
+++ lib/schedule_tweets_runner.rb
@@ -11,24 +11,28 @@ class ScheduleTweetsRunner
   end
 
   def run(buffer_schedule, username)
-    if buffer_schedule.empty?
-      LogEntry.log.info 'Buffer was empty, fetching tweets from contentful'
-
-      tweets = fetch_tweets
-
-      LogEntry.log.info "Attempting to add #{tweets.count} tweets to profile"
-      buffer_schedule.update(tweets)
-      buffer_schedule.shuffle
-
-      LogEntry.log.info "#{tweets.count} tweets successfully added to #{username} profile"
-    else
-      LogEntry.log.info "Buffer contains #{buffer_schedule.scheduled_tweets.total} tweets, doing nothing and exiting"
-    end
+    buffer_schedule.empty? ? update_and_shuffle_buffer(buffer_schedule, username) : do_nothing(buffer_schedule)
   end
 
   def fetch_tweets
     advertisements = HbsContent::ListOfTweets.advertisement.tweets.map(&:text)
     random_contents = HbsContent::ListOfTweets.internal_content.tweets.map(&:text).shuffle.take(6)
     advertisements.concat(random_contents)
+  end
+
+  def update_and_shuffle_buffer(buffer_schedule, username)
+    LogEntry.log.info 'Buffer was empty, fetching tweets from contentful'
+
+    tweets = fetch_tweets
+
+    LogEntry.log.info "Attempting to add #{tweets.count} tweets to profile"
+    buffer_schedule.update(tweets)
+    buffer_schedule.shuffle
+
+    LogEntry.log.info "#{tweets.count} tweets successfully added to #{username} profile"
+  end
+
+  def do_nothing(buffer_schedule)
+    LogEntry.log.info "Buffer contains #{buffer_schedule.scheduled_tweets.total} tweets, doing nothing and exiting"
   end
 end
